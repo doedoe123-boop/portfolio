e05a320766c230d11121364032b74f1bc578f14d
Nelson <asum.nelson@gmail.com>
2025-04-16 21:22:10 +0800

    updating color scheme

--- assets/vendor/js/scrolldown.js
+++ assets/vendor/js/scrolldown.js
@@ -1,4 +1,3 @@
 document.getElementById("scrollDown").addEventListener("click", function () {
   document.getElementById("about").scrollIntoView({ behavior: "smooth" });
 });
-document.getElementById("year").textContent = new Date().getFullYear();
